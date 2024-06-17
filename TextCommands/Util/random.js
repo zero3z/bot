ed4e033097b948d12f393dde8f72b7b62104d17b
zero3z <chimcon2023@gmail.com>
2024-06-18 00:42:19 +0700

    Update random.js

--- TextCommands/Util/random.js
+++ TextCommands/Util/random.js
@@ -12,7 +12,7 @@ module.exports = {
     }
     const random = parseInt(args[0])
     let rd = Math.floor(Math.random() * random) + 1;
-    await message.reply(`<a:foxiggle:1137472362342781000> **Số random của ${message.member} là:** __**${rd}**__ `);
+    await message.reply(`**Số random của ${message.member} là:** __**${rd}**__ `);
 
 
   }
