85087f5697aaa153dee60b975e2492266fa301b4
zero3z <chimcon2023@gmail.com>
2024-06-23 01:42:06 +0700

    Create donate.js

---
+++ TextCommands/Dev/donate.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "donate",
+  group: "dev",
+  aliases: ["d", "dnt"],
+  cooldown: 0,
+  description: "Hiển thị Số Tài Khoản để donate",
+  usage: "{prefix} donate",
+async execute(client, message, args) {
+   awai.message.reply('**­               BANK INFO ­ **\n\n**<:mbbank:1132201851945766913> MBBANK  : 7815102007\n<:momo:1132202298689454101> MOMO  : 0384542554\n\n*__Chủ Tài Khoản__* : TRAN LE MINH KHOI**'),
+}  
+
