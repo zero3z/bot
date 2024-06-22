9104885bb8ba7f4edd69c91c36457da34de6b624
zero3z <chimcon2023@gmail.com>
2024-06-23 01:44:17 +0700

    Update donate.js

--- TextCommands/Dev/donate.js
+++ TextCommands/Dev/donate.js
@@ -6,6 +6,6 @@ module.exports = {
   description: "Hiển thị Số Tài Khoản để donate",
   usage: "{prefix} donate",
 async execute(client, message, args) {
-   awai.message.reply('**­               BANK INFO ­ **\n\n**<:mbbank:1132201851945766913> MBBANK  : 7815102007\n<:momo:1132202298689454101> MOMO  : 0384542554\n\n*__Chủ Tài Khoản__* : TRAN LE MINH KHOI**')
+   awai.message.reply('**­               BANK INFO ­ **\n\n**<:mbbank:1132201851945766913> MBBANK  : 89922999999\n<:momo:1132202298689454101> MOMO  : 0384542554\n\n*__Chủ Tài Khoản__* : TRAN LE MINH KHOI**')
 };  
 
